709869e5f16f45f0c6cf21b6d97153a31b0e0475
Muhammad Uzair Arif <uzairarif28@gmail.com>
2025-09-10 02:24:39 +0500

    Project setup

---
+++ backend/EmployeeManagementSystem/Controllers/EmployeeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagementSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            // Logic to retrieve all employees
+            return Ok(new { Message = "List of all employees" });
+        }
+    }
+}
---
+++ backend/EmployeeManagementSystem/Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeManagementSystem.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagementSystem.Data
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        }
+}
