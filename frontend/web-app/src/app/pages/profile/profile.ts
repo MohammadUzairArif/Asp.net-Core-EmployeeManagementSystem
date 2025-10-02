506f197525dbdcd223454932aafa55d2aef7603a
user.name like : "uzairarif28@gmail.com <uzairarif28@gmail.com>
2025-10-03 02:57:09 +0500

    added image upload functionality

--- frontend/web-app/src/app/pages/profile/profile.ts
+++ frontend/web-app/src/app/pages/profile/profile.ts
@@ -1,7 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Auth } from '../../services/auth';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Eye, EyeClosed, LoaderCircle, LucideAngularModule } from 'lucide-angular';
+import { Eye, EyeClosed, LoaderCircle, LucideAngularModule, Pencil } from 'lucide-angular';
 import { Router } from '@angular/router';
 import { HttpService } from '../../services/http';
 
@@ -25,6 +25,7 @@ export class Profile implements OnInit {
   readonly LoaderCircle = LoaderCircle;
   readonly eye = Eye;
   readonly eyeClosed = EyeClosed;
+   readonly Pencil = Pencil;
 
   ngOnInit(): void {
     this.getProfile();
@@ -96,6 +97,20 @@ newPassword: [
       }
 
     });
+  }
+
+  onFileSelected(event: Event) {
+    const file = (event.target as HTMLInputElement).files?.[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      const base64 = reader.result as string;
+      this.profileForm.patchValue({
+        profileImage: base64
+      });
+    };
+    reader.readAsDataURL(file);
   }
 
 }
