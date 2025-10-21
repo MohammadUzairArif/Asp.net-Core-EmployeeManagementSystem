6530d416488fda3febb466414083f8e7605d7280
user.name like : "uzairarif28@gmail.com <uzairarif28@gmail.com>
2025-10-21 17:50:22 +0500

    implement admin dashboard

--- frontend/web-app/src/app/pages/home/home.ts
+++ frontend/web-app/src/app/pages/home/home.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Building2, CalendarDays, LucideAngularModule, Users, Wallet } from 'lucide-angular';
+import { DashboardService } from '../../services/dashboard-service';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
-export class Home {
+export class Home implements OnInit {
 
+  totalSalary!: number
+employeeCount!: number
+departmentCount!: number
+  // icons
+  readonly Users = Users;
+  readonly Building2 = Building2;
+ readonly  Wallet = Wallet;
+ readonly CalendarDays  = CalendarDays;
+
+ dashboardService = inject(DashboardService)
+
+  ngOnInit(): void {
+     this.getDashboardData(); 
+
+  }
+
+
+
+  getDashboardData() {
+   
+
+ 
+    this.dashboardService.getDashboardData().subscribe({
+      next: (result) => {
+
+        this.totalSalary = result.totalSalary
+        this.employeeCount = result.employeeCount
+        this.departmentCount = result.departmentCount
+      },
+      error: () => {
+        
+        alert('Failed to fetch dashboard data ');
+      }
+    });
+  }
 }
---
+++ frontend/web-app/src/app/services/dashboard-service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { IDashboard } from '../types/IDashboard';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashboardService {
+   http = inject(HttpClient);
+
+   getDashboardData(){
+      return this.http.get<IDashboard>(`${environment.apiUrl}/api/Dashboard`);
+    }
+}
